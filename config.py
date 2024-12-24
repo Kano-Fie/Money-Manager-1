a2f45cb0e64bb143135323448f891cb74f435d19
Kano-Fie <144331614+Kano-Fie@users.noreply.github.com>
2024-12-24 10:33:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,1 @@
-import os
 
-DEBUG = True
-# TESTING = True
-user="k12"
-password="Qwerty123"
-host="k12.mysql.database.azure.com"
-port=3306
-database="tracker"
-MYSQL_CURSORCLASS = 'DictCursor'
-SECRET_KEY = 'your_secret_key'
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'EMAIL_USER'
-MAIL_PASSWORD = 'EMAIL_PASS'
